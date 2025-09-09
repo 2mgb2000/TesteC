56197cbddca83ce1a1962f418104627ca4e8f17f
2mgb2000 <2mgb2000@proton.me>
2025-09-09 13:55:10 +0100

    new

--- aula5/ex_disciplinas.c
+++ aula5/ex_disciplinas.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 
 int main() {
@@ -23,15 +24,27 @@ int main() {
     getchar();
 
     for (int i = 0; i < numero_disc; i++) {
-            printf("Disciplina %d: ", i+1);
-            scanf(" %19[^\n]", disciplinas[i]);
-            //fgets(disciplinas[i], sizeof(disciplinas[i]), stdin);
-        }
+        printf("Disciplina %d: ", i+1);
+        //fgets(disciplinas[i], sizeof(disciplinas[i]), stdin);
+        // scanf(" %19[^\n]", disciplinas[i]);
 
+        if (fgets(disciplinas[i], sizeof(disciplinas[i]), stdin)) {
+        // remove newline if present
+            disciplinas[i][strcspn(disciplinas[i], "\n")] = '\0';
+
+            // if newline was not stored, clear leftover input
+            if (strchr(disciplinas[i], '\0') == &disciplinas[i][sizeof(disciplinas[i]) - 1]) {
+                int c;
+                while ((c = getchar()) != '\n' && c != EOF) { }
+            }
+        }
+    }    
+
+    
     // imprimir lista de disciplinas
     printf("\n\nDisciplinas inseridas:\n");
     for (int i = 0; i < numero_disc; i++) {
-        printf("Disciplina %i: %s\n", i+1, disciplinas[i]);
+        printf("%i. %s\n", i+1, disciplinas[i]);
     }
 
     return 0;
